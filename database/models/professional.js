e5dec990bdb67dfc8f2d931dc2a7f18d69aec0c1
ermi2021 <ermibling13@gmail.com>
2022-11-04 23:45:48 +0300

    models professional & role added

---
+++ database/models/professional.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class professional extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  professional.init({
+    name: DataTypes.STRING,
+    phone_number: DataTypes.STRING,
+    email: DataTypes.STRING,
+    role: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'professional',
+  });
+  return professional;
+};
