6b1dfbd6f2f7ed9004fdd70549acf68fbce74ceb
ermi2021 <ermibling13@gmail.com>
2022-11-04 23:39:17 +0300

    created migrations

---
+++ database/models/center.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class center extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  center.init({
+    name: DataTypes.STRING,
+    latitude: DataTypes.STRING,
+    longtiude: DataTypes.DOUBLE,
+    phone_number: DataTypes.STRING,
+    type: DataTypes.STRING,
+    email: DataTypes.STRING,
+    pobox: DataTypes.STRING,
+    manager_id: DataTypes.STRING,
+    director_id: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'center',
+  });
+  return center;
+};
---
+++ database/models/vent.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class vent extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  vent.init({
+    vent: DataTypes.STRING,
+    date: DataTypes.DATE,
+    time: DataTypes.TIME
+  }, {
+    sequelize,
+    modelName: 'vent',
+  });
+  return vent;
+};
