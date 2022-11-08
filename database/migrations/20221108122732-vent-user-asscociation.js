06c7e7a38c9c89684dc32c75cd7e061d8574bb11
ermi2021 <ermibling13@gmail.com>
2022-11-08 18:44:59 +0300

    added profession-role association

--- database/migrations/20221108122732-vent-user-asscociation.js
+++ database/migrations/20221108122732-vent-user-asscociation.js
@@ -3,20 +3,27 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  
+    await queryInterface.addColumn(
+      'vents',
+      'UserId',
+      {
+        type:Sequelize.INTEGER,
+        references:{
+          model:'users',
+          key:'id',
+        },
+        onUpdate:'CASCADE',
+        onDelete:'CASCADE'
+      }
+    )
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+      await queryInterface.removeColumn(
+        'vents',
+        'UserId'
+      )
+    
   }
 };
