d83f806fd1e579ad691185a641973523c6234d00
ermi2021 <ermibling13@gmail.com>
2022-11-14 14:56:00 +0300

    updateed professional - center association

--- database/models/professional.js
+++ database/models/professional.js
@@ -4,16 +4,14 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class professional extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
-      // define association here
-      professional.belongsTo(models.center);
-      professional.belongsToMany(models.role,{
-        through:'ProfessionalRole'
+
+      professional.belongsToMany(models.center, {
+        through: 'ProfessionalCenter'
+      });
+      professional.belongsToMany(models.role, {
+        through: 'ProfessionalRole'
       });
       professional.hasOne(models.contact);
     }
