a5725d7ef447c3b95bc6a4d739296af41372ef96
ermi2021 <ermibling13@gmail.com>
2022-11-23 23:58:57 +0300

    Added center seed

--- database/seeders/20221121203714-demo-center.js
+++ database/seeders/20221121203714-demo-center.js
@@ -4,25 +4,38 @@
 
 module.exports = {
 
-  async up (queryInterface, Sequelize) {
-   await queryInterface.bulkInsert('centers',[
-    {
-      id:1,
-      name: 'ABC',
-      latitude: "12.2564",
-      longtiude: "7.2564",
-      phone_number: "091215654658",
-      type: 'private',
-      email: "ermibling13@gmail.com",
-      pobox: 3920,
-    }
-   ]);
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('centers', [
+      {
+        id: 1,
+        name: 'ABC',
+        latitude: "12.2564",
+        longtiude: "7.2564",
+        phone_number: "091215654658",
+        type: 'private',
+        email: "ermibling13@gmail.com",
+        pobox: 3920,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        id: 2,
+        name: 'ERF',
+        latitude: "12.2564",
+        longtiude: "7.2564",
+        phone_number: "091215654658",
+        type: 'private',
+        email: "ermibling13@gmail.com",
+        pobox: 3920,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
 
   },
 
-  async down (queryInterface, Sequelize) {
-    
-     await queryInterface.bulkDelete('centers', null, {});
-  
+  async down(queryInterface, Sequelize) {
+
+    await queryInterface.bulkDelete('centers', null, {});
+
   }
 };
--- database/seeders/20221123182432-demo-professionalRole.js
+++ database/seeders/20221123182432-demo-professionalRole.js
@@ -18,7 +18,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    
      await queryInterface.bulkDelete('ProfessionalRole', null, {});
   }
 };
