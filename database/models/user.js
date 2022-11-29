16b84418bb6b18a9ec1774efc2145b15833417ab
ermi2021 <ermibling13@gmail.com>
2022-11-29 16:11:14 +0300

    Added results seed

--- database/models/user.js
+++ database/models/user.js
@@ -4,18 +4,14 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class user extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+    static associate(models) {  
       user.hasOne(models.contact);
       user.hasMany(models.vent);
       user.belongsToMany(models.center, {
         through: 'booked'
-      })
+      });
+      user.hasMany(model.chat);
+      user.hasMany(model.result);
     }
   }
   user.init({
